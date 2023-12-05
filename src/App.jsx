0b7cfe3138b3edd02e3286f439383dbcab34d218
Tiwari007 <vt2174781@gmail.com>
2023-12-05 23:10:52 +0530

    initial template

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import CreateDetails from './components/CreateDetails'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <div className='container'>
+        
+      {/* <Home /> */}
+        <CreateDetails />
+    </div>
+  )
+}
+
+export default App
